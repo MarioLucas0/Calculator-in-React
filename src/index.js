5a5e9165e2e94c75c31e5e3e0b9eedac0b87cb85
Lucas <mariolucas.2000@outlook.com>
2022-09-23 15:33:49 -0300

    Calculator almost finished

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Calculator from './main/Calculator';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>  
+    <Calculator/>
+  </React.StrictMode>
+);
+
+
